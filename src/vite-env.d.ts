fce3038993b6d468ebfb08092429da2c13c75a42
Roberto Ayala <rbt.ayala20@gmail.com>
2024-04-28 21:16:00 -0600

    feat(translations): declared module in vite env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,3 +3,8 @@ interface ImportMetaEnv {
   VITE_I18N_FALLBACK_LOCALE: string
   VITE_I18N_LOCALE: string
 }
+
+declare module 'virtual:i18next-loader' {
+  const resources: never
+  export default resources
+}

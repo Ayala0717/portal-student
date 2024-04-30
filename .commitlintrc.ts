6b63bb09cabdfa1b4aac6dd76d4a12bb8c3ba3c3
Roberto Ayala <rbt.ayala20@gmail.com>
2024-04-30 09:20:03 -0600

    chore(config): removed body leading blank rule that rule is used to subject and body does not have blank space

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -27,7 +27,8 @@ const Configuration: UserConfig = {
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'subject-case': [2, 'always', 'lower-case'],
-    'header-max-length': [2, 'always', 100]
+    'header-max-length': [2, 'always', 100],
+    'body-leading-blank': [0]
   },
   prompt: {
     settings: {},
